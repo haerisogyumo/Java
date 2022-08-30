29ee185298b7c370b4b6bb434c77daadfdd11eec
haerisogyumo <haerisogyumo@gmail.com>
2022-08-30 13:52:42 +0900

    /

--- Book1/src/Ch03/p78.java
+++ Book1/src/Ch03/p78.java
@@ -1,5 +1,20 @@
 package Ch03;
 
 public class p78 {
+	public static void main(String[] args) {
+		
+		int num1 =10;
+		int i = 2;
+		
+		boolean value = ((num1 = num1 + 10) < 10 ) && ((i = i + 2) < 10 );
+		System.out.println(value);
+		System.out.println(num1);
+		System.out.println(i);
+		
 
+		boolean value2 = ((num1 = num1 + 10) > 10 ) || ((i = i + 2) < 10 );
+		System.out.println(value);
+		System.out.println(num1);
+		System.out.println(i);
+	}
 }
